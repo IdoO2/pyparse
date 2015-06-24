fa1fc318ce906dee7447e2549443f953fa6fc5bc
dafp <dafpaterson@outlook.com>
2015-06-24 22:57:50 +0200

    Move parser I/O POC to external module
    + minor cleanup on view module

---
+++ parser.py
@@ -0,0 +1,76 @@
+class Parser():
+    'Simple POC for parser I/O'
+
+    def __init__(self, model):
+        self.data = model
+
+    # Example data, see `view`
+    map = [
+        [0, 0, 'Master | class'],
+        [-1, 0, 'randval | func'],
+        [-2, 1, '__init_ | func'],
+        None,
+        [-2, 0, 'change | func'],
+        None
+    ]
+
+    def loop(self):
+        'For testing purposes takes user input to update model'
+        while True:
+            update = input('Make a change (format: `3 def __init__(self, newarg):i`):')
+            lst = self.parseArgs(update)
+            if not lst: # There is nothing recorded on this line; obviously should change later
+                continue
+            line, val, rel_idx, loc_idx, idxs, txt = lst
+            if len(idxs) is 1:
+                if txt is not val:
+                    self.data.item(idxs[0]).setText(val)
+            else:
+                child = self.data.item(idxs[0]) # top level parent
+                for level in idxs[1:]:
+                    child = child.child(level)
+                if child.text() is not val:
+                    child.setText(val)
+
+    def parseArgs(self, update):
+        '''
+        Very basic arguments parsing:
+        input format is `3 some code`
+        where `3` is a line number and the rest is the content of the line
+        Returns
+        - int line number
+        - str val
+        - int rel_idx relation of line to previous lines
+        - in local_idx index of symbol in its scope
+        - idx idx actual index in list
+        - str txt redundant with val as long as parser doesn’t parse
+        '''
+        line = int(update[0])
+        val = update[2:]
+
+        if line >= len(self.map) or self.map[line] is None:
+            return False
+
+        rel_idx = int(self.map[line][0]) # <= 0
+        local_idx = int(self.map[line][1])
+        idxs = self.walkIndexes(line)
+        txt = self.map[line][2]
+        return line, val, rel_idx, local_idx, idxs, txt
+
+    def walkIndexes(self, line, *lst):
+        '''
+        Determine indexes down the hierarchy
+        rel_idx <= 0
+        Implementation note: given how python handles memory,
+        it probably isn’t necessary to pass idx_list around
+        '''
+        idx_list = lst[0] if len(lst) > 0 else []
+        if self.map[line][0] < 0: # relative index
+            parent_line = line + self.map[line][0]
+            self.walkIndexes(parent_line, idx_list)
+
+        idx_list += [self.map[line][1]] # local index
+
+        return idx_list
+
+
--- view.py
+++ view.py
@@ -1,21 +1,5 @@
 # Note on performance: in our case, consider setting `uniformRowHeights = True`
 
-# Test
-# This tree would be represented as
-# branch1
-#   b1s1
-#   b1s2
-#   subbranch1
-#     b1s1s1
-#     b1s1s2
-data = [
-    [
-        'branch1',
-        'b1s1', 'b1s2', ['subbranch1',
-            'b1s1s1', 'b1s1s2']
-    ]
-]
-
 # Example file:
 # 1. class Master():
 # 2.   randval = 0
@@ -30,6 +14,7 @@ data = [
             'change']]
 ]
 
+# Example mapping found in `parser`
 # We will need a mapping from file string to model,
 # for two reasons:
 # - when a line / line are updated, wee need to find the model reference easily
@@ -49,14 +34,7 @@ data = [
 # - update model at [3-2][1] accordingly
 # - lookup mapping for children of 3
 # - recurse on line 5
-mapping = [
-    [(0, 0, 'Master | class')],
-    [(-1, 0, 'randval | func')],
-    [(-2, 1, '__init_ | func')],
-    [None],
-    [(-2, 0, 'change | func')],
-    [None]
-]
+
 
 # Helpers
 import inspect
@@ -70,6 +48,7 @@ from PyQt5.QtCore import QDir, Qt, QStringListModel
 
 # Application
 from qmodel import Tree
+from parser import Parser
 
 class PyOutline(QMainWindow):
     '''
@@ -82,76 +61,6 @@ class PyOutline(QMainWindow):
         self.setGeometry(300, 300, 300, 150)
         self.setWindowTitle('Code browser')
 
-class Parser():
-    'Simple POC for parser I/O'
-    map = [
-        [0, 0, 'Master | class'],
-        [-1, 0, 'randval | func'],
-        [-2, 1, '__init_ | func'],
-        None,
-        [-2, 0, 'change | func'],
-        None
-    ]
-
-    def loop(self):
-        'For testing purposes takes user input to update model'
-        while True:
-            update = input('Make a change (format: `3 def __init__(self, newarg):i`):')
-            lst = self.parseArgs(update)
-            if not lst: # There is nothing recorded on this line; obviously should change later
-                continue
-            line, val, rel_idx, loc_idx, idxs, txt = lst
-            if len(idxs) is 1:
-                if txt is not val:
-                    data.item(idxs[0]).setText(val)
-            else:
-                child = data.item(idxs[0]) # top level parent
-                for level in idxs[1:]:
-                    child = child.child(level)
-                if child.text() is not val:
-                    child.setText(val)
-
-    def parseArgs(self, update):
-        '''
-        Very basic arguments parsing:
-        input format is `3 some code`
-        where `3` is a line number and the rest is the content of the line
-        Returns
-        - int line number
-        - str val
-        - int rel_idx relation of line to previous lines
-        - in local_idx index of symbol in its scope
-        - idx idx actual index in list
-        - str txt redundant with val as long as parser doesn’t parse
-        '''
-        line = int(update[0])
-        val = update[2:]
-
-        if line >= len(self.map) or self.map[line] is None:
-            return False
-
-        rel_idx = int(self.map[line][0]) # <= 0
-        local_idx = int(self.map[line][1])
-        idxs = self.walkIndexes(line)
-        txt = self.map[line][2]
-        return line, val, rel_idx, local_idx, idxs, txt
-
-    def walkIndexes(self, line, *lst):
-        '''
-        Determine indexes down the hierarchy
-        rel_idx <= 0
-        Implementation note: given how python handles memory,
-        it probably isn’t necessary to pass idx_list around
-        '''
-        idx_list = lst[0] if len(lst) > 0 else []
-        if self.map[line][0] < 0: # relative index
-            parent_line = line + self.map[line][0]
-            self.walkIndexes(parent_line, idx_list)
-
-        idx_list += [self.map[line][1]] # local index
-
-        return idx_list
-
 # Run Qt application
 if __name__ == '__main__':
     app = QApplication(sys.argv)
@@ -162,8 +71,8 @@ if __name__ == '__main__':
     data = Tree(data, 'Some file')
 
     # Test operations
-    data.addRow(1, 'NewBranch') # Test: add item at root index 1
-    data.addRow(0, 'NewSubBranch', data.item(1)) # Test: add items at item index 0 of root index 2
+    #data.addRow(1, 'NewBranch') # Test: add item at root index 1
+    #data.addRow(0, 'NewSubBranch', data.item(1)) # Test: add items at item index 0 of root index 2
 
     # View
     tree = QTreeView()
@@ -175,7 +84,7 @@ if __name__ == '__main__':
     ui.buildWindow(tree)
     ui.show()
 
-    run = Parser()
+    run = Parser(data)
     run.loop()
 
     sys.exit(app.exec_())
