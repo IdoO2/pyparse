c02e27cf547aa2c5389a3b4c951d6e45820fa50e
tuxcy <tuxcy17@gmail.com>
2015-08-20 03:34:26 +0200

    fixing multiline preprocess & end of line identification & identation analyze -- issue #11

--- src/test.py
+++ src/test.py
@@ -8,21 +8,21 @@ from parser.python_file import PythonFile
 from parser.db_toolkit import DBC
 
 if __name__ == '__main__':
-    for f in ['simple_oo.py', 'very_simple.py'] :
-        db = DBC()
+    # for f in ['simple_oo.py', 'very_simple.py', 'view.py'] :
+    for f in ['simple_oo.py'] :
         print (f)
-        fd = open(TEST_DIRECTORY + f, 'r')
-        test = PythonFile(f, TEST_DIRECTORY)
-        fd.close()
+        test = PythonFile()
+        test.process(f, TEST_DIRECTORY)
         for l in test.SCODE :
             if type(l) in [CodeLine] :
                 continue
+
             # print (l.show())
             # l.show()
         # #     # print(l.stype)
         print()
-        res = test.getSymbolTree()
-        print (res)
+        # res = test.getSymbolTree()
+        # print (res)
         #for x in res : print(x)
         print()
 
