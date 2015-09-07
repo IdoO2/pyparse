af994715aee8bf49ebb4dad3e0826fb21dda27a6
dafp <dafpaterson@outlook.com>
2015-09-08 01:16:54 +0200

    TMP Add install script for plugin
    /!\ With hard coded install path

---
+++ install_st.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+TARGET=$HOME'/.config/sublime-text-3/Packages'
+
+# Plugin contents to a zip
+#zip pyparse.sublime-plugin src/sublime_text/*.py src/sublime_text/PyParsePackage/
+
+# Create package dir
+cp -r src/sublime_text/PyParsePackage $TARGET
+
