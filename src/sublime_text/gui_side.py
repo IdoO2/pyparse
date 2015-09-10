83191fbf14b744d2e28f8dc469f7fcf1ea27b949
dafp <dafpaterson@outlook.com>
2015-09-10 23:39:25 +0200

    Drop dead code, clean server com, fix paths over http

--- src/sublime_text/gui_side.py
+++ src/sublime_text/gui_side.py
@@ -26,52 +26,27 @@ class SublimeServer(object) :
     def __init__ (self, UI) :
         self.UI = UI
 
-    def __process(self, data) :
-        filename = data[0]
-        filepath = data[1]
-        fullpath = filepath + '/' + filename
+    def __process(self, fullpath) :
+        filepath, filename = os.path.split(fullpath)
 
         self.UI.setWindowTitle(filename)
         self.UI.data = PythonFile()
         self.UI.data.process(filename, filepath + '/')
         self.UI.model.setFileName(fullpath)
         self.UI.model.setBranches(self.UI.data.getSymbolTree())
-        return "PROCESS OK"
-
-    def __update(self, data) :
-        filename = ''
-        filepath = ''
-        fullpath = filepath + '/' + filename
-
-        self.UI.data = PythonFile()
-        self.UI.data.process(filename, filepath + '/')
-        self.UI.model.setFileName(fullpath)
-        self.UI.model.setBranches(self.UI.data.getSymbolTree())
-        return "UPDT OK"
+        return '200'
 
     def serverProcess(self, idata, add_data) :
         """ Attempts to make sense of information from incoming requests
 
-            Data is space separated
             The first four characters of idata indicate expected operation
         """
-        idata.replace('\n', '')
-        action = idata[0:4]
-        qry = idata[4:]
+        action, query = idata[:4], idata[4:]
 
-        if action == "PROC":
-            # Expected: `filename full/path/to/file_folder`
-            # Fortunately file names never include spaces
-            spc_sep_parts = qry.split()
-            return self.__process([
-                spc_sep_parts[0],
-                ' '.join(spc_sep_parts[1:])
-            ])
-        elif action == "UPDT" :
-            return self.__update(qry.split())
-        else :
-            return "ERROR " + idata
+        if action != "PROC":
+            return '500'
+
+        return self.__process(query)
 
     def run(self) :
         NC.initTServer(1255, process=self.serverProcess)
-
