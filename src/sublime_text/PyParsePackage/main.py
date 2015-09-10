47da61fe7ce5bc981bf25ea2c708b673b082ebbc
dafp <dafpaterson@outlook.com>
2015-09-10 23:39:25 +0200

    Drop dead code, clean server com, fix paths over http

--- src/sublime_text/PyParsePackage/main.py
+++ src/sublime_text/PyParsePackage/main.py
@@ -4,56 +4,26 @@
 from threading import Thread
 import PyParsePackage.network_common as NC
 import sublime, sublime_plugin
+from os import path as ospath
 
 server = None
-
-
-def changeFile(data) :
-    file_path, file_name = data
-    s = NC.initClient("127.0.0.1", 1255)
-    qry = 'PROC' + file_path + ' ' + file_name
-    s.sendall((qry + '\n').encode('utf-8'))
-
-    rsp = s.recv(1024).decode()
-    if 'OK' in rsp.split() :
-        return True
-    return False
-    s.close()
-
-
-def lauchServer() :
-    global server
-    server = AnalyzerServerST()
-    server.run()
+OK_code = '200'
+ER_code = '500'
 
 class AnalyzerServerST(object) :
-    def __sync(self) :
-        self.isSync = True
-        return "SYNC OK"
-
     def __show(self, data) :
         view = sublime.active_window().active_view()
         target = view.text_point(int(data), 0)
         view.show(sublime.Region(target))
-        return "SHOW OK"
+        return OK_code
 
-    def __replace(self, data) :
-        return
+    def serverProcess(self, idata, unused):
+        action, qry = idata[:4], idata[4:]
 
-    def serverProcess(self, idata, add_data) :
-    # on récupère la donnée et on dirige les requêtes vers les bonnes fonctions
-        idata.replace('\n', '')
-        op = idata[0:4]
-        qry = idata[4:]
+        if action != 'SHOW':
+            return ER_code
 
-        if op == "SYNC" :
-            return self.__sync()
-        elif op == "SHOW" :
-            return self.__show(qry)
-        elif op == "REPL" :
-            return self.__replace(qry.split())
-        else :
-            return "ERROR " + idata
+        return self.__show(qry)
 
     def run(self) :
         NC.initTServer(1254, process=self.serverProcess)
@@ -62,19 +32,31 @@ class AnalyzerServerST(object) :
 class EventDump(sublime_plugin.EventListener):
     def on_activated(self, view):
         wdic = (view.window().extract_variables())
-        if "file_name" in wdic and '.py' in wdic["file_name"] :
+        if 'file_name' in wdic and '.py' == wdic['file_name'][-3:]:
             try:
-                changeFile([wdic["file_name"], wdic["file_path"]])
+                self.__changeFile(wdic['file_name'], wdic['file_path'])
             except Exception as em:
                 print ('PyParse error: {}'.format(em))
-                return
-        wdic = (view.window().extract_variables())
 
-    def on_post_save (self, view) :
-        pass
+    def __changeFile(self, file_name, file_path) :
+        fullpath = ospath.join(file_path, file_name)
+        query = 'PROC{}\n'.format(fullpath).encode('utf-8')
 
-if not NC.checkPort('127.0.0.1', 1254) :
+        try:
+            s = NC.initClient('127.0.0.1', 1255)
+            s.sendall(query)
+            rsp = s.recv(1024).decode()
+            s.close()
+        except Exception as em:
+            print('Unable to handle connection: {}'.format(em))
+
+
+if not NC.checkPort('127.0.0.1', 1254):
+    def lauchServer():
+        global server
+        server = AnalyzerServerST()
+        server.run()
+
     thd = Thread(target=lauchServer)
     thd.setDaemon(True)
     thd.start()
-
